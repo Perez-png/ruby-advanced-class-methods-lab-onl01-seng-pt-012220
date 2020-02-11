317e886b6f647436088e80a8abc95e591596c5e6
Xavier Perez <xperez007@gmail.com>
2020-02-11 16:37:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,5 +32,18 @@ Song.find_by_name("The
 Middle")
 end
 
+song_1 =
+Song.find_or_create_by_name("
+Blank Space")
+song_2 =
+Song.find_or_create_by_name("
+Blank Space")
+end
+
 
 end
+
+
+
+song_1 == song_2
+
